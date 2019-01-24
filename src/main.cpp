9be94d1389dfd740c8a3acac104b50d54d44349d
Karl Gumerlock <karl@gumerlock.com>
2019-01-23 20:01:26 -0800

    Added GPLv3 LICENSE

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+/*
+RaptorStarter -- A program to help learn programming using the
+Arcbotics Sparki platform.
+
+Copyright (C) 2017  Alexander Lin
+          (C)       Karl Gumerlock
+          (C)       Guillermo Gomez
+          (C)       Iyal Suresh
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
 #include <Raptor.h>
 #include <SPI.h>
 
